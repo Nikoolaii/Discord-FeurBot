da9112e64c89f59a106a99866e2591c83837f88d
Nikoolaii <nikolailemerre@gmail.com>
2022-07-28 18:14:01 +0200

    Finish bot

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ from dotenv import load_dotenv
 
 intents = nextcord.Intents.default()
 intents.message_content = True
+intents.guilds = True
 
 load_dotenv()
 
 bot = commands.Bot(command_prefix='', intents=intents)
 
+
 @bot.event
 async def on_ready():
     result = pyfiglet.figlet_format(f'{bot.user}')
     print(result)
-    await bot.change_presence(status=nextcord.Status.idle, activity = nextcord.Activity(type=nextcord.ActivityType.watching, name=f"quoi sur {len(bot.guilds)} serveurs"))
+    await bot.change_presence(status=nextcord.Status.idle,
+                              activity=nextcord.Activity(type=nextcord.ActivityType.watching,
+                                                         name=f"quoi sur {len(bot.guilds)} serveurs"))
 
 
 @bot.event
@@ -28,12 +32,16 @@ async def on_message(message):
 
 @bot.event
 async def on_guild_remove():
-    await bot.change_presence(status=nextcord.Status.idle, activity = nextcord.Activity(type=nextcord.ActivityType.watching, name=f"quoi sur {len(bot.guilds)} serveurs"))
+    await bot.change_presence(status=nextcord.Status.idle,
+                              activity=nextcord.Activity(type=nextcord.ActivityType.watching,
+                                                         name=f"quoi sur {len(bot.guilds)} serveurs"))
 
 
 @bot.event
 async def on_guild_join():
-    await bot.change_presence(status=nextcord.Status.idle, activity = nextcord.Activity(type=nextcord.ActivityType.watching, name=f"quoi sur {len(bot.guilds)} serveurs"))
+    await bot.change_presence(status=nextcord.Status.idle,
+                              activity=nextcord.Activity(type=nextcord.ActivityType.watching,
+                                                         name=f"quoi sur {len(bot.guilds)} serveurs"))
 
 
 bot.run(os.getenv("BOT_TOKEN"))
